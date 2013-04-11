55e94229d454e6ff1d8ae293cf680432fbe0c8a6
Appixia <info@appixia.com>
2013-04-11 12:46:43 +0300

    improved page hook message

--- pagehook.php
+++ pagehook.php
@@ -33,8 +33,11 @@ $url = CartAPI_Handlers_Helpers::getShopDomain() . $_REQUEST['q'];
 
 <iframe src="bridge://SendMessageToParent/Url?<?php echo urlencode($url); ?>" width="0" height="0" frameborder="0"></iframe>
 
-<p>Appixia page hook - you are seeing this because you have the "appixia" cookie. Did you use the debugger?</p>
+<p>The Appixia Prestashop module has intercepted the display of this page.</p>
+<p>You are seeing this because you have the "appixia" cookie (did you use the debugger?) or you're using an appixia mobile app.</p>
 <p><a href="pagehookclear.php?url=<?php echo urlencode($url); ?>">Return to original website (clear cookie)</a></p>
+<br>
+<p>If you're trying to show a page from your store website in your mobile app <a href="http://kb.appixia.com/cart:plugin-overrides-prestashop:cms-page">read this</a>.</p>
 <br><br>
 <p>Original requested url:<br><?php echo $url ?></p>
 
